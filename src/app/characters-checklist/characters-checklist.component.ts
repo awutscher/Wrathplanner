b00b618173151e98fa0d396b4a105067d799e570
Alexander Wutscher <alexander.wutscher@chello.at>
2023-01-03 13:40:32 +0100

    shenanigans

---
+++ src/app/characters-checklist/characters-checklist.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { CharacterfunctionService } from '../characterfunction.service';
+
+
+@Component({
+  selector: 'app-characters-checklist',
+  templateUrl: './characters-checklist.component.html',
+  styleUrls: ['./characters-checklist.component.scss']
+})
+export class CharactersChecklistComponent implements OnInit {
+
+  constructor(
+    private service: CharacterfunctionService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+}
