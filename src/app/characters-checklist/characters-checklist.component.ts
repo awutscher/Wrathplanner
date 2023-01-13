a8c640daf167b212a588c720914147928ef76864
Alexander Wutscher <alexander.wutscher@chello.at>
2023-01-13 13:12:56 +0100

    *ngFor issue fixed

--- src/app/characters-checklist/characters-checklist.component.ts
+++ src/app/characters-checklist/characters-checklist.component.ts
@@ -1,12 +1,13 @@
+import { NgModule } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
 import { ArrayType } from '@angular/compiler';
 import { asNativeElements, Component, OnInit } from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { CharacterfunctionService } from '../characterfunction.service';
-import { Icharacters } from '../Icharacters';
 import { Directive, ElementRef, Input, OnChanges, Sanitizer, SecurityContext,
   SimpleChanges } from '@angular/core';
-import { Pipe, PipeTransform } from '@angular/core';
+import { Icharacters } from '../Icharacters';
+
 
 
 
@@ -18,37 +19,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 
 export class CharactersChecklistComponent implements OnInit {
-  /* elementRef: any; */
-
+  characters:any = [];
   
   constructor(
     public CharacterfunctionService: CharacterfunctionService,
-    /* public characters: ElementRef */
-  ) {
-    /* this.elementRef. */ /* var characters = this.CharacterfunctionService.allStorage()
-    console.log(characters)
-    console.log(Array.isArray(characters)) */
-    /* characters = characters as Icharacters[] */
-    
-  }
+  ) {}
 
-  
-  ngonChange():void{
-    /* var characters:any[] = this.CharacterfunctionService.allStorage();
-    console.log(characters)
-    let chars:Icharacters[] = characters
-    console.log(characters) */
-  }
   ngOnInit(): void {
-    var characters = this.CharacterfunctionService.allStorage()
-    /* console.log(characters) */
-    console.log(Array.isArray(characters))
-    /* characters = characters as Icharacters[] */
-    console.log(characters)
-    
-    /* console.log(chars) */
-    /* console.log(typeof(characters)) */
-    /* console.log(typeof(Object.entries(characters))) */
-    /* console.log(typeof(chars)) */
+    this.characters = this.CharacterfunctionService.allStorage()
+    console.log(Array.isArray(this.characters))
+    console.log(this.characters)
   } 
 }
